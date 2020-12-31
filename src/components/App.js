251462c548b125461d2decfb901385a4797e8c02
Johan Castillo <jcjohan2707@gmail.com>
2020-12-30 23:50:38 -0400

    Dependencies Update

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Route} from 'react-router-dom';
 import Menu from './Menu'
 import Inventario from './inventario/index'
 import Tienda from './tienda/index'
+
+
 const App = () => (
   <div className="main-container">  
     <BrowserRouter>
+
       <div className="header">
         <Menu/>
       </div>
+
       <div className='content'>
         <Route exact path='/' component={Tienda}/>
         <Route exact path='/inventario' component={Inventario}/>
-    
       </div>
+
     </BrowserRouter>
   </div>
 );
